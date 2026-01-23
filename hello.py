4f8f8a64fd4e66f1d5c4eb5b00d4740559358b78
Shreya Porwal <porwalshreya11111@gmail.com>
2026-01-24 00:07:53 +0530

    .

--- hello.py
+++ hello.py
@@ -18,15 +18,3 @@ def hello_world():
     print("hello")
 
 hello_world()
-
-# What I did right
-# I think the graph approach would work  and I'll have to map years between movie up and down
-# Namra said we need to get up time of movie and add
-
-# What I did wrong
-# I didn't ask questions
-# I didn't question my approach I thought it must be related to what I have learned
-
-#yes
-#yes
-
